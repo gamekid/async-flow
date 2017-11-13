9f986cc927434d7600b90de9ab1b9c6e4a6bf925
nselof <selof86@gmail.com>
2017-11-13 11:32:34 -0600

    Major Redesign: Changed the data structure for the routine so it wouldn't require a response property for every action. Cleaned up the conceptual language of the class, a routine has multiple steps with actions in them. Added helper functions to assist in building the routine data structure

--- index.js
+++ index.js
@@ -1,94 +1,168 @@
 'use strict';
 
 class Flow {
+  
+  /////////////////////////////////////////////// ROUTINE BUILDING METHODS ////
 
-  constructor(data, callback) {
-    if (typeof callback !== 'function') 
-      callback = false;
-    if (typeof data !== 'undefined')
-      this.async(data, callback);
+  constructor(routine) {
+    this.routine = Array.isArray(routine) ? routine : [];
+    this.currentActions = null;
+    // setup aliases
+    this.first = this.after = this.last = this.then;
+    this.and = this.with = this.action;
   }
 
-  async(data, callback) {
-    this.data = data;
-    this.responses = {};
-    this.isCompleted = false;
-    this.promise = new Promise( (resolve, reject) => {
-      // save resolve and reject actions for other methods
-      this.resolve = (response) => {
-        // perform non-promise callback if necessary
-        if (typeof callback === 'function')
-          callback(false, response);
+  then(keyOrFn, possibleFn) {
+    this.wait();
+    return this.and(keyOrFn, possibleFn);
+  }
 
-        resolve(response);
-      };
-      this.reject = (err) => {
-        // perform non-promise callback if necessary
-        if (typeof callback === 'function')
-          callback(err);
+  wait(){
+    // make a new group of actions
+    this.currentActions = [];
+    this.routine.push(this.currentActions);
+  }
 
-        reject(err);
-      };
-      // start the steps
-      this.processStep(0);
-    });
+  action(keyOrFn, possibleFn) {
+    // add this action to current group
+    if (typeof possibleFn === 'function') {
+      let step = {};
+      step[keyOrFn] = possibleFn;
+      this.currentActions.push(step);
+    } else {
+      this.currentActions.push(keyOrFn);
+    }
     return this;
   }
 
-  exec(data) { // returns the promise
-    if (typeof data !== 'undefined')
-      this.async(data);
+  //////////////////////////////////////////// RESPONSE PROCESSING METHODS ////
 
+  filterResponses(filterResponsesObject) {
+    this.filterResponsesObject = filterResponsesObject;
+    return this;
+  }
+  doFilterResponses() {
+    if (typeof this.filterResponsesObject === 'undefined') {
+      return;
+    }
+
+    let newResponses = {};
+    for (let key in this.responses) {
+      if (this.filterResponsesObject.hasOwnProperty(key))
+        newResponses[key] = typeof this.filterResponsesObject[key] === 'function' ?
+          this.responses[key].filter(this.filterResponsesObject[key]) :
+          this.responses[key];
+    }
+    this.responses = newResponses;
+  }
+  mapResponses(mapResponsesObject) {
+    this.mapResponsesObject = mapResponsesObject;
+    return this;
+  }
+  doMapResponses() {
+    if (typeof this.mapResponsesObject === 'undefined') {
+      return;
+    }
+
+    let newResponses = {};
+    for (let key in this.responses) {
+      if (this.mapResponsesObject.hasOwnProperty(key))
+        newResponses[key] = typeof this.mapResponsesObject[key] === 'function' ?
+          this.responses[key].map(this.mapResponsesObject[key]) :
+          this.responses[key];
+    }
+    this.responses = newResponses;
+  }
+
+  ///////////////////////////////////////////// ACTION PROCESSING ////
+
+  exec() { // returns a promise
+    this.responses = {};
+    this.completion = [];
+    this.isComplete = false;
+    this.promise = new Promise( (resolve, reject) => {
+      // save resolve and reject actions for other methods
+      this.resolve = () => {
+        this.isComplete = true;
+        this.doFilterResponses();
+        this.doMapResponses();
+        return resolve(this.responses);
+      }
+      this.reject = err => {
+        this.isComplete = true;
+        return reject(err);
+      }
+      // start the steps
+      this.doStep(0);
+    });
     return this.promise;
   }
 
-  processStep(index) {
-    // check if previous step is completed
+  doStep(index) {
+    if (this.isComplete) {
+      return;
+    }
+
+    // check if previous step (group of actions) is completed
     if (index != 0) {
-      let prevGroup = this.data[index-1];
-      for (let item in prevGroup) {
-        // return if entry is not found in this.responses for each item
-        if (typeof this.responses[item] === 'undefined')
+      for (let isActionComplete of this.completion[index-1]) {
+        if (!isActionComplete) {
           return;
+        }
       }
     }
 
-    // resolve if this step doesn't exist, we're done
-    if (index == this.data.length)
-      return this.resolve(this.responses);
-
-    // get next group in data
-    let group = this.data[index];
-
-    // reject if group is not object
-    if (typeof group !== 'object') 
-      return this.reject("Entry is not an Object");
+    // if this step doesn't exist, we're done
+    if (index == this.routine.length) {
+      return this.resolve();
+    }
 
     // setup next step callback
-    let nextStep = () => this.processStep(index + 1);
+    let nextStep = () => this.doStep(index + 1);
 
-    // execute current step
-    this.processGroup(group, nextStep);
+    // get actions for current step
+    let actions = this.routine[index];
+
+    // execute actions in current step
+    this.doActions(actions, nextStep);
   };
 
-  processGroup(group, nextStep) {
-    for (let item in group) {
-      if (typeof group[item] === 'function') {
-        // if item is a function, assume it returns a promise
-        group[item]()
+  doActions(actions, nextStep) {
+    let stepCompletion = [];
+    this.completion.push(stepCompletion);
+    for (let action of actions) {
+      // get and check the function
+      let fn = typeof action === 'object' ? action[Object.keys(action)[0]] : action;
+
+      if (typeof fn !== 'function') {
+        this.reject('Action is not a Function');
+      }
+
+      // run the function
+      let possiblePromise = fn(this.responses);
+
+      // check if no response
+      if (typeof possiblePromise === 'undefined') {
+        stepCompletion.push(true);
+        return nextStep();
+      } else {
+        stepCompletion.push(false);
+        // convert response into a promise (if it isn't already)
+        let definitelyPromise = Promise.resolve(possiblePromise);
+        definitelyPromise
           .then( (response) => {
-            this.responses[item] = response;
-            nextStep();
+            // mark as complete
+            stepCompletion[actions.indexOf(action)] = true;
+            // should we put this in responses?
+            if (typeof action === 'object') {
+              this.responses[Object.keys(action)] = response;
+            }
+            return nextStep();
           })
           .catch(this.reject);
-      } else {
-        // if not a function, just dump the data directly into responses and call nextStep
-        this.responses[item] = group[item];
-        nextStep();
       }
     }
   };
-
 };
 
 exports = module.exports = Flow;
